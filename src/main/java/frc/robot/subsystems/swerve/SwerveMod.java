e4b8defb496dbe1da325346f3e9346b0d971558c
TopherHeim <99445655+TopherHeim@users.noreply.github.com>
2024-12-10 10:49:56 -0500

    Update SwerveMod.java

--- src/main/java/frc/robot/subsystems/swerve/SwerveMod.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveMod.java
@@ -93,6 +93,13 @@ public class SwerveMod implements SwerveModule
         
     }
     public void resetToAbsolute() {
+    try {
+        Thread.sleep(200); // Add a 200 ms delay
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+
+ 
     double absolutePosition = getCanCoder().getDegrees();  // Get CANCoder absolute position
     double zeroedPosition = absolutePosition - angleOffset.getDegrees();  // Apply offset
     relAngleEncoder.setPosition(zeroedPosition);  // Reset motor encoder position
